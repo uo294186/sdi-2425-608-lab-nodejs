b66fa55a5f4d9509a62b5f6614ec9bf5a6ad8f7a
UO294186 <uo294186@uniovi.es>
2025-03-11 12:34:25 +0100

    SDI-2425-608-sesión8-ejercicio-complementario2

--- musicctoreapp/app.js
+++ musicctoreapp/app.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 require("./routes/songs.js")(app);
+require("./routes/authors.js")(app);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
