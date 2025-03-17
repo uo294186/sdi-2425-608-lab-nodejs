00b382b2cbc4e04e5d6be50b8c9982ea1f94bd07
uo294186 <uo294186@uniovi.es>
2025-03-17 17:47:57 +0100

    SDI-2425-608-9.4-Registro de usuario, autenticación y uso de sesión

--- musicctoreapp/app.js
+++ musicctoreapp/app.js
@@ -5,15 +5,24 @@ let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
 let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
 
 let app = express();
+let expressSession = require("express-session");
+app.use(expressSession({
+  secret:"abcdefg",
+  resave: true,
+  saveUninitialized: true
+}));
+
+let crypto = require("crypto");
 let fileUpload = require("express-fileupload");
 app.use(fileUpload({
   limits:{fileSize:50*1024*1024},
   createParentPath:true
 }));
 app.set("uploadPath", __dirname);
+app.set("clave", "abcdefg");
+app.set("crypto", crypto);
 
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -28,6 +37,10 @@ songsRepository.init(app, dbClient);
 require("./routes/songs.js")(app, songsRepository);
 require("./routes/authors.js")(app);
 
+const usersRepository = require("./repositories/usersRepository.js");
+usersRepository.init(app, dbClient);
+require("./routes/users.js")(app, usersRepository);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'twig');
@@ -39,7 +52,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- musicctoreapp/routes/songs.js
+++ musicctoreapp/routes/songs.js
@@ -40,6 +40,10 @@ module.exports=function (app, songsRepository){
     });
 
     app.get('/songs/add', function(req, res) {
+        if(req.session.user == null){
+            res.redirect("/shop");
+            return;
+        }
         res.render("songs/add.twig");
     });
 
@@ -51,10 +55,17 @@ module.exports=function (app, songsRepository){
     });
 
     app.post('/songs/add', function (req,res){
+
+        if (req.session.user == null){
+            res.redirect("/shop");
+            return;
+        }
+
         let song = {
             title: req.body.title,
             kind: req.body.kind,
-            price: req.body.price
+            price: req.body.price,
+            author:req.session.user
         }
         songsRepository.insertSong(song, function(result){
             if(result.songId !== null && result.songId !== undefined){
